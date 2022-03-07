994302657000edf6d825519547cd0a179584e8f5
RS Studio <61602042+Pantal-pl@users.noreply.github.com>
2022-03-07 17:53:33 +0100

    Add files via upload

---
+++ bubbleSort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int tablica[] = {1,2,5,8,2,5,5,1,6,1,5,7,4,1,4}; // nasza tablica
+    bubbleSort(tablica); // wywolanie funkcji z nasza tablica
+    for(int i = 0;i < 15 ;i++) {
+        printf("%d",tablica[i]); // petla wypisujaca wszystkie posortowane elementy tablicy
+    }
+}
+    void bubbleSort(int tab[]) { // deklaracja funkcji z argumentem
+        for(int i=0; i<14;i++) { // 14 to rozmiar tablicy, nie 11 bo ostatni element jest juz posortowany
+            for(int j=0;j<14-i;j++) {
+                if(tab[j]>tab[j+1]) { // sprawdzanie czy pierwszy element jest wiekszy niz drugi
+                    int temp = tab[j]; // przypisanie elementu tablicy do zmiennej temp
+                    tab[j] = tab[j+1]; // zmiana wartosci elementu z nastepnym
+                    tab[j+1] = temp; // zamiana kolejnego elementu tablicy na wartosc temp
+                }
+            }
+        }
+    }
---
+++ insertionSort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int tablica[] = {1,5,2,22,5,4,15,1,4,14,2,8,4,2,6}; // nasza tablica
+    insertionSort(tablica); // wywolanie funkcji z nasza tablica
+    for(int i = 0;i < 15 ;i++) {
+        printf("%3d",tablica[i]); // petla wypisujaca wszystkie elementy tablicy
+    }
+}
+    void insertionSort(int tab[]) { // deklaracja funkcji z argumentem
+        for(int i = 1; i < 15;i++) {
+            int temp = tab[i]; // zapisywanie drugiego elementu do zmiennej temp
+            int j = i - 1;
+            while(j>=0 && tab[j] > temp) { //sparwdzanie czy elementy po lewej stronie sa wieksze od temp
+                tab[j+1] = tab[j];
+                j--;
+                tab[j+1] = temp; // przesuniecie w prawo
+            }
+        }
+    }
